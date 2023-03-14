20de98fd0c613497dcebe2ae8721d9c08051ebea
dave-mccollough <davemccollough@gmail.com>
2023-03-13 22:10:31 -0500

    Functions

---
+++ 1-fundamentals/j_control_flow/notes.md
@@ -0,0 +1,10 @@
+# Control Flow
+
+- Order in which statements are ran
+- Includes conditionals and loops
+
+- If statements
+    - start statement with `if` keyword
+    
+
+
---
+++ 1-fundamentals/j_control_flow/target/debug/j_control_flow.d
@@ -0,0 +1,1 @@
+/Users/davemccollough/Source/learning-rust/1-fundamentals/j_control_flow/target/debug/j_control_flow: /Users/davemccollough/Source/learning-rust/1-fundamentals/j_control_flow/src/main.rs
