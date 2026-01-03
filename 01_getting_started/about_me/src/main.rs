265cbebf0bbd8e7a5df526ebd6ef2b9f16566f83
Dave McCollough <davemccollough@gmail.com>
2026-01-02 23:40:36 -0700

    Getting started

---
+++ 01_getting_started/about_me/src/main.rs
@@ -0,0 +1,36 @@
+/*
+Create a new `about_me` project with the `cargo new` command.
+ 
+Using the `println!` macro, output 3 sentences about yourself.
+Feel free to invoke the macro multiple times.
+ 
+From the Terminal, compile the `main.rs` file inside the `src`
+folder with the Rust compiler, then manually run the executable.
+ 
+From the Terminal, compile the project with the Cargo tool, then
+manually run the executable.
+ 
+From the Terminal, compile and run the project with a single
+Cargo command.
+ 
+Check your program for errors with `cargo check`.
+ 
+Add a comment at the top of your source code explaining how to
+compile the program for new Rust developers.
+ 
+Add some spaces and line breaks to the code so that it is formatted
+in an ugly manner. From the Terminal, style the code with the
+`cargo fmt` command.
+ 
+Replace the `println!` macro with `print!`. What happens?
+
+Use Cargo Run to run project
+*/
+
+
+
+fn main() {
+    println!("My name is Dave.");
+    println!("I enjoy mountain biking, climbing, and trail running.");
+    println!("I am learning Rust in 2026.");
+}
